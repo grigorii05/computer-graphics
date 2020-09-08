9d1ea4aa60df2ff3ac138ead9416639a0e920e88
Aleksander Vorobiev <avorobiev@cybertonica.com>
2020-09-08 19:24:22 +0300

    add lab01 example

---
+++ lab01/index.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById('canvas')
+const context = canvas.getContext('2d')
+
+const len = 100
+const tMax = 500
+let t = 0
+
+function update() {
+    context.fillStyle = '#FF6677'
+    context.clearRect(0, 0, canvas.width, canvas.height)
+
+    for (let i = 0; i < len; i++) {
+        context.fillRect(10 + i + t, 10 + i, 10, 1)
+    }
+
+    if (t > tMax)
+        t = 0
+    else
+        t++
+}
+
+setInterval(update, 1)
+
+setTimeout(() => {
+    t = 0
+}, 1)
