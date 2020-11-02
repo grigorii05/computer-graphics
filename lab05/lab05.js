84d07d809749399b80cd7efa0a49410388baaaa2
Aleksander Vorobiev <avorobiev@cybertonica.com>
2020-11-02 12:25:36 +0300

    add lab05

---
+++ lab05/lab05.js
@@ -0,0 +1,11 @@
+// TODO:
+// 1) user prints N-edge figure
+// 2) user prints line
+// 3)
+
+const canvas = document.getElementById('canvas')
+const context = canvas.getContext('2d')
+
+list = []
+const min = Math.min(...list);
+const max = Math.max(...list);
